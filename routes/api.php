3df44beede40285b4d37255936174520c905de2e
bruno.bhab <bruno.henrique@beedoo.com.br>
2024-06-02 18:58:07 -0300

    chore: pequenos ajustes

--- routes/api.php
+++ routes/api.php
@@ -4,11 +4,16 @@ use App\Http\Controllers\Api\Auth\AuthApiController;
 use App\Http\Controllers\Api\PermissionController;
 use App\Http\Controllers\Api\PermissionUserController;
 use App\Http\Controllers\Api\UserController;
+use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Route;
+
+Route::get('/', fn () => response()->noContent(Response::HTTP_OK))->name('index');
+Route::get('/unauthorized', fn () => response()->json(['message' => 'unauthorized']))->name('unauthorized');
 
 Route::get('/me', [AuthApiController::class, 'me'])->middleware('auth:sanctum')->name('auth.me');
 Route::post('/logout', [AuthApiController::class, 'logout'])->middleware('auth:sanctum')->name('auth.logout');
 Route::post('/auth', [AuthApiController::class, 'auth'])->name('auth.login');
+
 
 Route::middleware(['auth:sanctum', 'acl'])->group(function () {
     Route::apiResource('/permissions', PermissionController::class);
